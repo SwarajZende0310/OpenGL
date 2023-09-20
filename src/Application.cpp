7dfa00f2cb38c7c0c03d7417e0876c83c865edec
SwarajZende0310 <zendeswaraj53@gmail.com>
2023-09-20 11:57:17 +0530

    Adding test menu

--- src/Application.cpp
+++ src/Application.cpp
@@ -62,20 +62,32 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        test::TestClearColor test;
+        test::Test* currentTest=nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
+
+        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
+            GLCall(glClearColor(0.f, 0.f, 0.f, 1.f));
             /* Render here */
             renderer.Clear();
 
-            test.OnUpdate(0.f);
-            test.OnRender();
-
             ImGui_ImplGlfwGL3_NewFrame();
 
-            test.OnImGuiRender();
+            if (currentTest) {
+                currentTest->OnUpdate(0.f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-")) {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
+            }
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
--- src/tests/test.h
+++ src/tests/test.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include<iostream>
+#include<vector>
+#include<functional>
+#include<string>
+
 namespace test {
 	class Test {
 	public:
@@ -9,5 +14,21 @@ namespace test {
 		virtual void OnUpdate(float deltaTime){}
 		virtual void OnRender() {}
 		virtual void OnImGuiRender(){}
+	};
+
+	class TestMenu : public Test {
+	public:
+		TestMenu(Test*&currentTestPointer) ;
+
+		void OnImGuiRender()override;
+
+		template<typename T>
+		void RegisterTest(const std::string& name) {
+			std::cout << "Registering test " << name << std::endl;
+			m_Tests.push_back({ name,[]() {return new T(); } });
+		}
+	private:
+		Test*& m_CurrentTest;
+		std::vector < std::pair<std::string, std::function<Test* ()>>>m_Tests;
 	};
 }
