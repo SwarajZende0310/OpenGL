8fa4a8cfef7faa081f73c34e1926a6980bd4588a
SwarajZende0310 <zendeswaraj53@gmail.com>
2023-09-22 14:52:39 +0530

    Added some Set Uniforms

--- src/Shader.h
+++ src/Shader.h
@@ -25,7 +25,9 @@ public:
 	//Set uniforms
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform1i(const std::string& name, int value);
-	void SetUniform4f(const std::string& name,float v0, float v1, float f2, float f3 );
+	void SetUniform2f(const std::string& name, float v0, float v1);
+	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
+	void SetUniform4f(const std::string& name,float v0, float v1, float v2, float v3 );
 	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 
 private:
